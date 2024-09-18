e224ec7dfff03fe7a774735e23b0b6b0dc1a9d5a
AymaneOnline <138299321+AymaneOnline@users.noreply.github.com>
2024-09-18 21:37:52 +0100

    My Solution

--- resources/views/rows.blade.php
+++ resources/views/rows.blade.php
@@ -20,14 +20,20 @@
                         </thead>
                         <tbody>
                             @foreach ($users as $user)
+                                @if ($loop->count == 0)
+                                    <tr>
+                                        <td>No Content</td>
+                                    </tr>
+                                @else
                                 {{-- Task: only every second row should have "bg-red-100" --}}
-                                <tr class="bg-red-100">
-                                    <td>{{-- Task: add row number here: 1, 2, etc. --}}</td>
+                                <tr class="{{ $loop->even ? 'bg-red-100' : '' }}">
+                                    <td>{{ $loop->iteration }}</td>
                                     <td>{{ $user->name }}</td>
                                     {{-- Task: only the FIRST row should have email with "font-bold" --}}
-                                    <td class="font-bold">{{ $user->email }}</td>
+                                    <td class="{{ $loop->first ? 'font-bold' : '' }}">{{ $user->email }}</td>
                                     <td>{{ $user->created_at }}</td>
                                 </tr>
+                                @endif
                             @endforeach
                         </tbody>
                     </table>
